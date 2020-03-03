0b27ff408af4ecd3bbbe056251b91e7816e832f6
Ubuntu <vagrant@ubuntu-xenial.localdomain>
2020-03-03 02:32:40 +0000

    added mockaroo product data

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcryptjs = require('bcryptjs');
 const config = require('./config.json');
+const product = require('./products.json');
 
 const port = 3000;
 
 
 //connect to db
-// const mongodbURI = 'mongodb+srv://pearlydb:P3arly@pearly-cpl-yceqt.mongodb.net/test?retryWrites=true&w=majority'
+// const mongodbURI = 'mongodb+srv://pearlydb:<password>@pearly-cpl-yceqt.mongodb.net/test?retryWrites=true&w=majority'
 const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority`;
 mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('DB connected!'))
@@ -26,6 +27,27 @@ db.once('open', function() {
 });
 
 
+app.use((req,res, next)=>{
+  console.log(`${req.method} request for ${req.url}`);
+  next(); //include this to go to the next middleware
+});
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/allProducts',(req, res)=>{
+  res.json(product);
+});
+
+
+app.get('/product/p=:id',(req,res)=>{
+  const idParam = req.params.id;
+  for (let i = 0; i < product.length; i++){
+    if (idParam.toString() === product[i].id.toString()) {
+      res.json(product[i]);
+    }
+  }
+});
+
+
+// keep this alwasy at the bottom so that you can see the errors reported
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
