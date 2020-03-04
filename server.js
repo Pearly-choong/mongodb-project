0ab5e1a217e2bce6040a920ddd0f8cf64b32224a
Pooi lai Choong <choongpl@gmail.com>
2020-03-04 15:22:49 +1300

    added user.js and authentication login

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const bcryptjs = require('bcryptjs');
-const config = require('./config.json');
-const product = require('./products.json');
+const bodyParser = require('body-parser'); // to parse all data coming from the user and db
+const cors = require('cors'); //to include cross origin request
+const bcryptjs = require('bcryptjs'); //to hash and compare password in an encryted form
+const config = require('./config.json'); //to store credentials
+const product = require('./products.json'); //external json data from mockaroo api
+const dbProduct = require('./models/products.js');
+const User = require('./models/users.js');
 
 const port = 3000;
 
 
 //connect to db
 // const mongodbURI = 'mongodb+srv://pearlydb:<password>@pearly-cpl-yceqt.mongodb.net/test?retryWrites=true&w=majority'
-const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}.mongodb.net/test?retryWrites=true&w=majority`;
+const mongodbURI = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@${config.MONGO_CLUSTER_NAME}.mongodb.net/shop?retryWrites=true&w=majority`;
 mongoose.connect(mongodbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(() => console.log('DB connected!'))
 .catch(err => {
@@ -32,6 +34,11 @@ app.use((req,res, next)=>{
   next(); //include this to go to the next middleware
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(cors());
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/allProducts',(req, res)=>{
@@ -47,6 +54,53 @@ app.get('/product/p=:id',(req,res)=>{
     }
   }
 });
+
+app.post('/registerUser', (req,res)=> {
+ // checking if user is found in the db already
+  User.findOne({username:req.body.username},(err, userResult)=> {
+    if (userResult){
+      res.send('username taken already. Please try another one');
+    } else {
+      const hash = bcryptjs.hashSync(req.body.password); //has the password
+      const user = new User({
+        _id : new mongoose.Types.ObjectId,
+        username : req.body.username,
+        email : req.body.email,
+        password : hash
+      });
+      //save to database and notify the user accordingly
+      user.save().then(result => {
+        res.send(result);
+      }).catch(err => res.send(err));
+    }
+  })
+
+
+});
+
+// get all user
+app.get('/allUsers', (req, res)=> {
+  User.find().then(result => {
+    res.send(result);
+  });
+});
+
+
+//login the user
+app.post('/loginUser',(req,res) =>{
+  User.findOne({username: req.body.username}, (err, userResult) =>{
+    if (userResult){
+      if(bcryptjs.compareSync(req.body.password, userResult.password)){
+        res.send(userResult);
+      } else {
+        res.send('not authorized');
+      } //inner if
+    } else {
+      res.send('user not found. Please register');
+  } //outer if
+}); //findOne
+}); //post
+
 
 
 // keep this alwasy at the bottom so that you can see the errors reported
